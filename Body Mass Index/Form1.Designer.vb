11d3cad4f16883e24fa768fb9a636de3b5c287fd
Monde Castro <45668982+Monde-Castro@users.noreply.github.com>
2022-08-04 16:16:58 +0200

    Added files via upload

---
+++ Body Mass Index/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBMI
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblNormal = New System.Windows.Forms.Label()
+        Me.lblOverweight = New System.Windows.Forms.Label()
+        Me.lblObese = New System.Windows.Forms.Label()
+        Me.lblUnderweight = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtLength)
+        Me.GroupBox1.Controls.Add(Me.txtWeight)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(30, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(412, 126)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Member Details"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(152, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Weight in Kilograms:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(169, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Length in Centimeters:"
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Location = New System.Drawing.Point(213, 38)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(128, 26)
+        Me.txtWeight.TabIndex = 2
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(213, 70)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(128, 26)
+        Me.txtLength.TabIndex = 3
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.GroupBox2.Controls.Add(Me.lblUnderweight)
+        Me.GroupBox2.Controls.Add(Me.lblObese)
+        Me.GroupBox2.Controls.Add(Me.lblOverweight)
+        Me.GroupBox2.Controls.Add(Me.lblNormal)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(30, 209)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(412, 196)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Body Mass Index"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(468, 247)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(105, 28)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(468, 312)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(105, 30)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(30, 159)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(412, 28)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "C&alculate Body Mass Index"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(78, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Below 18:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(39, 79)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 20)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "18 to below 25:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(39, 144)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 20)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "30 and above:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(39, 113)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(116, 20)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "25 to below 30:"
+        '
+        'lblNormal
+        '
+        Me.lblNormal.BackColor = System.Drawing.SystemColors.Control
+        Me.lblNormal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblNormal.Location = New System.Drawing.Point(172, 78)
+        Me.lblNormal.Name = "lblNormal"
+        Me.lblNormal.Size = New System.Drawing.Size(96, 23)
+        Me.lblNormal.TabIndex = 4
+        Me.lblNormal.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblOverweight
+        '
+        Me.lblOverweight.BackColor = System.Drawing.SystemColors.Control
+        Me.lblOverweight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOverweight.Location = New System.Drawing.Point(172, 112)
+        Me.lblOverweight.Name = "lblOverweight"
+        Me.lblOverweight.Size = New System.Drawing.Size(96, 21)
+        Me.lblOverweight.TabIndex = 5
+        Me.lblOverweight.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblObese
+        '
+        Me.lblObese.BackColor = System.Drawing.SystemColors.Control
+        Me.lblObese.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblObese.Location = New System.Drawing.Point(172, 143)
+        Me.lblObese.Name = "lblObese"
+        Me.lblObese.Size = New System.Drawing.Size(96, 27)
+        Me.lblObese.TabIndex = 6
+        Me.lblObese.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblUnderweight
+        '
+        Me.lblUnderweight.BackColor = System.Drawing.SystemColors.Control
+        Me.lblUnderweight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblUnderweight.Location = New System.Drawing.Point(172, 38)
+        Me.lblUnderweight.Name = "lblUnderweight"
+        Me.lblUnderweight.Size = New System.Drawing.Size(96, 28)
+        Me.lblUnderweight.TabIndex = 7
+        Me.lblUnderweight.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'frmBMI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(585, 417)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmBMI"
+        Me.Text = "Body Mass Index"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtLength As System.Windows.Forms.TextBox
+    Friend WithEvents txtWeight As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblUnderweight As System.Windows.Forms.Label
+    Friend WithEvents lblObese As System.Windows.Forms.Label
+    Friend WithEvents lblOverweight As System.Windows.Forms.Label
+    Friend WithEvents lblNormal As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+
+End Class
---
+++ Body Mass Index/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class frmBMI
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear label and textboxes
+        txtLength.Clear()
+        txtWeight.Clear()
+        lblUnderweight.Text = " "
+        lblNormal.Text = " "
+        lblOverweight.Text = " "
+        lblObese.Text = " "
+
+        txtWeight.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Declare variables
+        Dim decMemberWeight As Decimal = 0
+        Dim decMemberLength As Decimal = 0
+        Dim decBMI As Decimal = 0
+
+        'Input 
+        decMemberLength = CDec(txtLength.Text)
+        decMemberWeight = CDec(txtWeight.Text)
+
+        'Processing
+        'Calculate the body mass index
+        decBMI = CDec(decMemberWeight / (decMemberLength / 100) ^ 2)
+
+        If decBMI < 18 Then
+            lblUnderweight.Text = "Underweight "
+        Else
+            lblUnderweight.Text = " "
+        End If
+        If (decBMI >= 18 And decBMI < 25) Then
+            lblNormal.Text = "Normal "
+        Else
+            lblNormal.Text = " "
+        End If
+        If (decBMI >= 25 And decBMI < 30) Then
+            lblOverweight.Text = " Overweight"
+        Else
+            lblOverweight.Text = " "
+        End If
+        If decBMI >= 30 Then
+            lblObese.Text = " Obese"
+        Else
+            lblObese.Text = " "
+
+        End If
+    End Sub
+End Class
